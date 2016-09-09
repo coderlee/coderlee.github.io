bc4b29a6e44511cbad5743b5a973a980dc8000e2
Lee <lee@lee.zj.cn>
2016-09-09 11:47:42 +0800

    add books/

---
+++ books/CSS/count.js
@@ -0,0 +1,5 @@
+document.write('<div style="display:none;">')
+document.write('<script language="JavaScript" src="http://s83.cnzz.com/stat.php?id=456125&web_id=456125&show=pic" charset="gb2312" type="text/javascript"></scr'+'ipt>')
+document.write('<script language="javascript" type="text/javascript" src="http://js.users.51.la/986842.js"></'+'script>')
+document.write('<script language="javascript" src="http://count34.51yes.com/click.aspx?id=345133463&logo=1"></'+'script>')
+document.write('</div>')
